bba0955c93ffe9cf176ccd7176a8bf3b52074e45
3zzinup <wintiger98@gmail.com>
2024-01-04 23:13:36 +0900

    123

--- src/countries/posts/router.py
+++ src/countries/posts/router.py
@@ -14,5 +14,35 @@ router = APIRouter(
 
 
 @router.post("")
-async def detst(country_id: int, db: Session = Depends(get_db)):
+async def post_posts(country_id: int, db: Session = Depends(get_db)):
     return country_id
+
+
+@router.get("")
+async def get_all_posts(country_id: int, db: Session = Depends(get_db)):
+    pass
+
+
+@router.get("/{post_id}")
+async def get_post(country_id: int, post_id: int, db: Session = Depends(get_db)):
+    pass
+
+
+@router.put("/{post_id}")
+async def put_post(country_id: int, post_id: int, db: Session = Depends(get_db)):
+    pass
+
+
+@router.delete("/{post_id}")
+async def delete_post(country_id: int, post_id: int, db: Session = Depends(get_db)):
+    pass
+
+
+@router.post("/{post_id}/like")
+async def like_post(country_id: int, post_id: int, db: Session = Depends(get_db)):
+    pass
+
+
+@router.post("/{post_id}/dislike")
+async def dislike_post(country_id: int, post_id: int, db: Session = Depends(get_db)):
+    pass
