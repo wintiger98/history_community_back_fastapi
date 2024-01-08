1992972ef7ca86994eb32573bd4488c6b15b2f64
3zzinup <wintiger98@gmail.com>
2024-01-08 22:38:51 +0900

    123

--- src/countries/posts/router.py
+++ src/countries/posts/router.py
@@ -33,7 +33,9 @@ async def get_all_posts(country_id: int, db: Session = Depends(get_db)):
     # 1. 국가 확인
     country = get_country_by_id(country_id=country_id, db=db)
     # 2. 게시글 조회
-    pass
+    filters = {"country_id": country.id}
+    posts = get_posts(filters=filters, db=db)
+    return posts
 
 
 @router.get("/{post_id}")
@@ -41,15 +43,23 @@ async def get_post(country_id: int, post_id: int, db: Session = Depends(get_db))
     # 1. 국가 확인
     country = get_country_by_id(country_id=country_id, db=db)
     # 2. 게시글 조회
-    pass
+    filters = {"id": post_id}
+    post = get_posts(filters=filters, db=db)
+    return post[0]
 
 
 @router.put("/{post_id}")
-async def put_post(country_id: int, post_id: int, db: Session = Depends(get_db)):
+async def put_post(
+    country_id: int, post_id: int, data: PostInput, db: Session = Depends(get_db)
+):
     # 1. 국가 확인
     country = get_country_by_id(country_id=country_id, db=db)
     # 2. 게시글 조회
+    filters = {"id": post_id}
+    post = get_posts(filters=filters, db=db)
+    post = post[0]
     # 3. 게시글 수정(작성자 여부 확인)
+    post = update_post(data=data, post=post, db=db)
     pass
 
 
@@ -58,8 +68,15 @@ async def delete_post(country_id: int, post_id: int, db: Session = Depends(get_d
     # 1. 국가 확인
     country = get_country_by_id(country_id=country_id, db=db)
     # 2. 게시글 조회
+    filters = {"id": post_id}
+    post = get_posts(filters=filters, db=db)
+    post = post[0]
     # 3. 게시글 삭제(작성자 여부 확인)
-    pass
+    result = erase_post(post=post, db=db)
+    if not result:
+        raise HTTPException(400, detail="Delete failed")
+    else:
+        return {"result": True, "detail": "Delete success"}
 
 
 @router.post("/{post_id}/like")
@@ -67,6 +84,9 @@ async def like_post(country_id: int, post_id: int, db: Session = Depends(get_db)
     # 1. 국가 확인
     country = get_country_by_id(country_id=country_id, db=db)
     # 2. 게시글 확인
+    filters = {"id": post_id}
+    post = get_posts(filters=filters, db=db)
+    post = post[0]
     # 3. 좋아요
     pass
 
@@ -76,5 +96,8 @@ async def dislike_post(country_id: int, post_id: int, db: Session = Depends(get_
     # 1. 국가 확인
     country = get_country_by_id(country_id=country_id, db=db)
     # 2. 게시글 확인
+    filters = {"id": post_id}
+    post = get_posts(filters=filters, db=db)
+    post = post[0]
     # 3. 싫어요
     pass
